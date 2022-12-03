53cf80d66d599d51a1b1233df1f4842b0d3d503a
KetrinXD <seregaopky@mail.ru>
2022-12-03 10:22:03 +0500

    Squashed commit of the following:
    commit 923d85bd8d34804c4fae2eb626d2768be110dbfa
    Author: Vantany <119051119+Vantany@users.noreply.github.com>
    Date:   Sat Dec 3 10:16:42 2022 +0500
    
        add form
    
    commit edbeb46b340cc502371235a29812755c4b210315
    Author: Vantany <119051119+Vantany@users.noreply.github.com>
    Date:   Sat Dec 3 10:05:51 2022 +0500
    
        Squashed commit of the following:
    
        commit 17df24f6442b053a6d9f482b1bc787fd128f2472
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 10:01:16 2022 +0500
    
            fixes
    
        commit 819e5aab2f3c163b31b78f6d5a298bd30de990ba
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:52:48 2022 +0500
    
            added user data to proposal
    
        commit 8d5e69b11fb2d648e34a4d104f69d94622776030
        Author: oleg-zhukov <zhukov.ol3q@mail.ru>
        Date:   Sat Dec 3 09:33:46 2022 +0500
    
            Update main.db
    
        commit 429032d4a05434fd0c5f1e2427ebe2077c949917
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:25:54 2022 +0500
    
            main.db
    
        commit f68073c17588905666337f2a537b4bc92b2b2751
        Merge: 98260b1 1da3ac4
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:23:36 2022 +0500
    
            Merge branch 'main' of https://github.com/oleg-zhukov/hakaton
    
        commit 98260b162ce0f647ff90dc1a63ceedcc941799e4
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:22:42 2022 +0500
    
            db fix
    
        commit 1da3ac494f2897d3843b6395a816f39bab767494
        Author: oleg-zhukov <zhukov.ol3q@mail.ru>
        Date:   Sat Dec 3 09:17:08 2022 +0500
    
            fix
    
        commit 7aa0c4e7205322f4dbdcb37dfdeb82e2cb6886bc
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:08:07 2022 +0500
    
            Clerify
    
    commit 057b0938bd01071a6bbc9c2deeca8ae2734bd4b3
    Author: Vantany <119051119+Vantany@users.noreply.github.com>
    Date:   Sat Dec 3 09:56:36 2022 +0500
    
        Squashed commit of the following:
    
        commit 819e5aab2f3c163b31b78f6d5a298bd30de990ba
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:52:48 2022 +0500
    
            added user data to proposal
    
        commit 8d5e69b11fb2d648e34a4d104f69d94622776030
        Author: oleg-zhukov <zhukov.ol3q@mail.ru>
        Date:   Sat Dec 3 09:33:46 2022 +0500
    
            Update main.db
    
        commit 429032d4a05434fd0c5f1e2427ebe2077c949917
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:25:54 2022 +0500
    
            main.db
    
        commit f68073c17588905666337f2a537b4bc92b2b2751
        Merge: 98260b1 1da3ac4
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:23:36 2022 +0500
    
            Merge branch 'main' of https://github.com/oleg-zhukov/hakaton
    
        commit 98260b162ce0f647ff90dc1a63ceedcc941799e4
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:22:42 2022 +0500
    
            db fix
    
        commit 1da3ac494f2897d3843b6395a816f39bab767494
        Author: oleg-zhukov <zhukov.ol3q@mail.ru>
        Date:   Sat Dec 3 09:17:08 2022 +0500
    
            fix
    
        commit 7aa0c4e7205322f4dbdcb37dfdeb82e2cb6886bc
        Author: KetrinXD <seregaopky@mail.ru>
        Date:   Sat Dec 3 09:08:07 2022 +0500
    
            Clerify
    
    commit b352ba89e229110328a047204171257f5510022b
    Author: Vantany <119051119+Vantany@users.noreply.github.com>
    Date:   Sat Dec 3 09:54:03 2022 +0500
    
        1

---
+++ forms/addproposalform.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, RadioField
+from wtforms.validators import DataRequired
+
+
+"""Разобраться с полями"""
+class AddProposalForm(FlaskForm):
+
+    #В словарь
+    fullname = StringField('Введите ФИО', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    number = StringField('Введите ваш телефон', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    post = StringField('Введите вашу должность', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    place_of_work = StringField('Введите место работы', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    theme = StringField('Введите тему работы', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    title = StringField('Введите заголовок работы', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    annotation = StringField('Введите аннотацию по конкурсной работе', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+
+
+    #Передаю в базу
+    type = RadioField('Выберите тип заявки', choices=[('text','Печатное издание'),('video','Видеоматериал')])
+    file = StringField('Введите ссылку на файл в облачном хранилище', validators=[DataRequired(message="Поле 'адрес' не может быть пустым")])
+    user_data = {
+        "fullname": fullname,
+        "number": number,
+        "post": post,
+        "place of work": place_of_work,
+        "theme": theme,
+        "title": title,
+        "annotation": annotation,
+    }
+    
+    submit = SubmitField('Добавить сообщение')
